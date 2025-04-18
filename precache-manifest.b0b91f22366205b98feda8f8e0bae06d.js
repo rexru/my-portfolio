eff647e2982ec1898e0ec2c5c87cb423c3bc5303
Rushil <saharushil05@gmail.com>
2025-04-18 15:30:44 -0400

    Updates

---
+++ precache-manifest.b0b91f22366205b98feda8f8e0bae06d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a6e1c59024624b2b5bf86794a83454",
+    "url": "/rexru/my-portfolio.git/index.html"
+  },
+  {
+    "revision": "081271e167a0fee1d16d",
+    "url": "/rexru/my-portfolio.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "72a285ad32e52f16b27e",
+    "url": "/rexru/my-portfolio.git/static/js/2.efdab03d.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/rexru/my-portfolio.git/static/js/2.efdab03d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ad4c0f9a4119b86ecf3",
+    "url": "/rexru/my-portfolio.git/static/js/3.6ce5ce29.chunk.js"
+  },
+  {
+    "revision": "081271e167a0fee1d16d",
+    "url": "/rexru/my-portfolio.git/static/js/main.8d603a18.chunk.js"
+  },
+  {
+    "revision": "0bcb3d0042effb894920",
+    "url": "/rexru/my-portfolio.git/static/js/runtime-main.28171d99.js"
+  }
+]);
